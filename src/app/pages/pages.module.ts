b42092529818e0fbd89b4b6e091735f59515f17d
Alejandro <alejandroconsuegra8@gmail.com>
2021-03-09 07:32:40 -0500

    pagina de graficas1

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,34 +5,41 @@ import { SharedModule } from '../shared/shared.module';
 
 import { FormsModule } from "@angular/forms";
 
+
 import { PagesComponent } from "./pages.component";
 
 
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { Graficas1Component } from "./graficas1/graficas1.component";
 import { ProgressComponent } from "./progress/progress.component";
+import { Graficas1Component } from "./graficas1/graficas1.component";
+
+//ng2-charts
+import { ChartsModule } from 'ng2-charts';
 
 //temporal
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
 
 
 @NgModule({
     declarations:[
-        PagesComponent,
         DashboardComponent,
         ProgressComponent,
         Graficas1Component,
-        IncrementadorComponent
+        PagesComponent,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports:[
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
     ],
     imports:[
         SharedModule,
         PAGES_ROUTES,
-        FormsModule
+        FormsModule,
+        ChartsModule,
     ]
   
 })
